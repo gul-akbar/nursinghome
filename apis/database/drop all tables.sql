621399c714814a48fcdaf76bd274d31fc6e77b4b
Gul Akbar (personal) <gul.akbar.3@gmail.com>
2023-07-22 17:50:03 +0100

    initial db creation

---
+++ apis/database/drop all tables.sql
@@ -0,0 +1,7 @@
+
+drop table dbo.ErrorLog;
+drop table dbo.[Session];
+drop table dbo.AppointmentSessionTemplate;
+drop table dbo.AppointmentSession;
+drop table dbo.NursingHome;
+drop table dbo.Contact;
